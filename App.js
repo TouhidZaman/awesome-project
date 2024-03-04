f7807615a4261168ab0f4bb63ad5fc3d6b1b4152
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2024-03-04 17:01:29 +0600

    SafeAreaProvider added

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import { styles } from './App.style';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import {
+	SafeAreaProvider,
+	useSafeAreaInsets,
+} from "react-native-safe-area-context";
+import { styles } from "./App.style";
+
+function HomeScreen() {
+	const insets = useSafeAreaInsets();
+	return (
+		<View
+			style={{
+				...styles.container,
+				paddingTop: insets.top,
+				paddingBottom: insets.bottom,
+			}}
+		>
+			<Text style={styles.subtitle}>Yee! It's working</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.subtitle}>This is my First React Native App...</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<SafeAreaProvider>
+			<HomeScreen />
+		</SafeAreaProvider>
+	);
 }
--- App.style.js
+++ App.style.js
@@ -1,15 +1,13 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#000",
-    alignItems: "center",
-    // justifyContent: 'center',
-    paddingTop: 50,
-  },
-  subtitle: {
-    color: "white",
-    fontSize: 20,
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#000",
+		alignItems: "center",
+	},
+	subtitle: {
+		color: "white",
+		fontSize: 20,
+	},
 });
